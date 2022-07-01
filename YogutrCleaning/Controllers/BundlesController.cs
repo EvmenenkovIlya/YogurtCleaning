e3968aec772c3929dd1cac79e7dd63d89e3e1cd0
alkanik <alkanik@gmail.com>
2022-07-01 09:43:59 +0300

    bundles contrioller & models

--- YogutrCleaning/Controllers/BundlesController.cs
+++ YogutrCleaning/Controllers/BundlesController.cs
@@ -1,44 +1,70 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using YogurtCleaning.Infrastructure;
+using YogurtCleaning.Models;
 
-namespace YogurtCleaning.Controllers
+namespace YogurtCleaning.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("[controller]")]
+public class BundleController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class PacketController : ControllerBase
+    private readonly ILogger<BundleController> _logger;
+
+    public BundleController(ILogger<BundleController> logger)
     {
-        private readonly ILogger<PacketController> _logger;
+        _logger = logger;
+    }
+    [AllowAnonymous]
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(BundleResponce), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<BundleResponce> GetBundle(int id)
+    {
+        return Ok(new BundleResponce());
+    }
+    [AuthorizeRoles(Role.Admin)]
+    [HttpGet]
+    [ProducesResponseType(typeof(List<BundleResponce>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public ActionResult<List<BundleResponce>> GetAllBundles()
+    {
+        return Ok(new List<BundleResponce>());
+    }
 
-        public PacketController(ILogger<PacketController> logger)
-        {
-            _logger = logger;
-        }
+    [AuthorizeRoles(Role.Admin)]
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+    public ActionResult UpdateBundle([FromBody] BundleRequest bundle, int id)
+    {
+        return NoContent();
+    }
 
-        [HttpGet("{id}")]
-        public Bundle GetBundle(int id)
-        {
-            return new Bundle();
-        }
+    [AuthorizeRoles(Role.Admin)]
+    [HttpPost]
+    [ProducesResponseType(typeof(int), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+    public ActionResult<int> AddBundle([FromBody] BundleRequest bundle)
+    {
+        int bundleId = new BundleResponce().Id;
+        return Created($"{Request.Scheme}://{Request.Host.Value}{Request.Path.Value}/{bundleId}", bundleId);
+    }
 
-        [HttpGet]
-        public List<Bundle> GetAllBundles()
-        {
-            return new List<Bundle>();
-        }
-        [HttpPut("{id}/{name}/{priceForRoom}/{priceForBathroom}/{priceForSquareMeter}/{services}")]
-        public void UpdateBundle(int id, string name, decimal priceForRoom, decimal priceForBathroom, decimal priceForSquareMeter, List<Service> services)
-        {
-        }
-        [HttpPost("{name}/{priceForRoom}/{priceForBathroom}/{priceForSquareMeter}/{services}")]
-        public int AddBundle(string name, decimal priceForRoom, decimal priceForBathroom, decimal priceForSquareMeter, List<Service> services)
-        {
-            return new Bundle().Id;
-        }
-
-        [HttpDelete("{id}")]
-        public int DeleteBundle(int id)
-        {
-            return new Bundle().Id;
-        }
+    [AuthorizeRoles(Role.Admin)]
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult DeleteBundle(int id)
+    {
+        return Ok();
     }
 }
