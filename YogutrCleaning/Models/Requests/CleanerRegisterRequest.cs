5c35df24f5d905a1f96d97a873f52a861140e0e2
Eugen <phlosof@gmail.com>
2022-07-02 19:24:58 +0300

    add attributes to CleanerRegisterRequest

---
+++ YogutrCleaning/Models/Requests/CleanerRegisterRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YogurtCleaning.Infrastructure;
+
+namespace YogurtCleaning.Models
+{
+    public class CleanerRegisterRequest
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = ApiErrorMessages.PasswordIsRequired)]
+        [MinLength(8)]
+        [MaxLength(50)]
+        public string Password { get; set; }
+        [Required]
+        [MinLength(8)]
+        [MaxLength(50)]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}
---
+++ YogutrCleaning/Models/Requests/CleanerUpdateRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace YogurtCleaning.Models.Requests
+{
+    public class CleanerUpdateRequest
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Phone { get; set; }
+    }
+}
