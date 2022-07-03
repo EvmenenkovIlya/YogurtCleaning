9efdfca5dddf4bd9a33b62601f39019c5b8f51f4
alkanik <alkanik@gmail.com>
2022-07-03 21:32:54 +0300

    add data annotation for comment request model

--- YogutrCleaning/Infrastructure/ApiErrorMessages.cs
+++ YogutrCleaning/Infrastructure/ApiErrorMessages.cs
@@ -9,9 +9,9 @@ public static class ApiErrorMessages
     public const string BirthDateIsRequired = "BirthDay was not entered";
     public const string EmailIsRequired = "Email was not entered";
     public const string PhoneIsRequired = "Phone was not entered";
-        //public const string
-        //public const string
-        //public const string
+    public const string AuthorIdIsRequred = "Author's Id was not entered";
+    public const string OrderIdIsRequred = "Order's Id was not entered";
+    public const string RatingIsRequred = "Rating from 1 to 5 was not entered";
         //public const string
         //public const string
         //public const string
