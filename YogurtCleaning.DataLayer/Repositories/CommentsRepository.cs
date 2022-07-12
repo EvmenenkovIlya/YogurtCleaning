1b45272f5d780de625fe4e61036c63150952d69d
alkanik <alkanik@gmail.com>
2022-07-12 22:38:53 +0300

    add comments repository

---
+++ YogurtCleaning.DataLayer/Repositories/CommentsRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YogurtCleaning.DataLayer.Entities;
+
+namespace YogurtCleaning.DataLayer.Repositories;
+
+public class CommentsRepository : ICommentsRepository
+{
+    private readonly YogurtCleaningContext _context;
+
+    public CommentsRepository(YogurtCleaningContext context)
+    {
+        _context = context;
+    }
+
+    public int AddCommentt(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteCommentt(Comment comment)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Comment>? GetAllComments()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ YogurtCleaning.DataLayer/Repositories/Interfaces/ICommentsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YogurtCleaning.DataLayer.Entities;
+
+namespace YogurtCleaning.DataLayer.Repositories;
+
+public interface ICommentsRepository
+{
+    List<Comment>? GetAllComments();
+    int AddCommentt(Comment comment);
+    void DeleteCommentt(Comment comment);
+}
