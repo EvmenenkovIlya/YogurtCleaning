e4b23f1fb0404e6379381d56c1a7ce1ae3f51046
Ilya <Evmenya@gmail.com>
2022-07-14 23:13:45 +0300

    Delete: interfaces namespace

--- YogurtCleaning.Business/Services/ServicesService.cs
+++ YogurtCleaning.Business/Services/ServicesService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using YogurtCleaning.DataLayer.Entities;
-using YogurtCleaning.DataLayer.Repositories.Interfaces;
+using YogurtCleaning.DataLayer.Repositories;
 
 namespace YogurtCleaning.Business.Services;
 
