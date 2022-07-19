1f62050d7254c9de0d7fd739a5b7cef1f4d4faea
Ilya <Evmenya@gmail.com>
2022-07-19 10:09:49 +0300

    fix server options

--- YogutrCleaning/Infrastructure/ServerOptions.cs
+++ YogutrCleaning/Infrastructure/ServerOptions.cs
@@ -4,5 +4,5 @@ public class ServerOptions
 {
     private const string _localServer = @"Server=.;Database=YogurtCleaning.DB;Trusted_Connection=True;";
     private const string _networkServer = @"Server=80.78.240.16;Database=YogurtCleaning.DB;User Id = Student; Password=qwe!23;";
-    public const string ConnectionOption = _localServer;
+    public const string ConnectionOption = _networkServer;
 }
