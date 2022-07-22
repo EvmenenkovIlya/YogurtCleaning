db5a88c9459e579463ffaa96bd15f8afba9b7db6
Ilya <Evmenya@gmail.com>
2022-07-22 20:01:07 +0300

    Delete if in program for swagger
    It was delete for publish API

--- YogutrCleaning/Program.cs
+++ YogutrCleaning/Program.cs
@@ -74,15 +74,10 @@ builder.Services.AddScoped<IBundlesService, BundlesService>();
 
 builder.Services.AddAutoMapper(typeof(MapperConfigStorage));
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
