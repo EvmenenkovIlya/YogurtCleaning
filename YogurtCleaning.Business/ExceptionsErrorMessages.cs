7d9427b6959d081a986bd0153e051e259876bfb3
Ilya <Evmenya@gmail.com>
2022-07-25 14:04:11 +0300

    Add delete order endpoint

--- YogurtCleaning.Business/ExceptionsErrorMessages.cs
+++ YogurtCleaning.Business/ExceptionsErrorMessages.cs
@@ -4,6 +4,8 @@ public static class ExceptionsErrorMessages
 {
     public const string ClientNotFound = "Client with this requestId not found";
     public const string CleanerNotFound = "Cleaner with this requestId not found:";
+
+    public const string OrderNotFound = "Order with this requestId not found:";
     public const string ClientOrdersNotFound = "Orders for Client with this requestId not found:";
     public const string CleanerOrdersNotFound = "Order for Cleaner with this requestId not found:";
     public const string ClientCommentsNotFound = "Comments for Client with this requestId not found:";
